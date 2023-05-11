a2a6c1e6510bdb6cc4d174b3ba0a51b3ee19eb03
gdivovic <goran.divovic@audatex.com>
2023-05-11 22:02:23 +0200

    Add model classes

---
+++ src/main/java/com/example/appointmentapp/model/Appointment.java
@@ -0,0 +1,59 @@
+package com.example.appointmentapp.model;
+
+import java.util.Date;
+
+/**
+ * @author goran.divovic
+ */
+public class Appointment {
+
+    private String id;
+
+    private String title;
+
+    private Date date;
+
+    private String location;
+
+    private String detail;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
---
+++ src/main/java/com/example/appointmentapp/model/PublicHoliday.java
@@ -0,0 +1,92 @@
+package com.example.appointmentapp.model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author goran.divovic
+ */
+public class PublicHoliday {
+
+    private Date date;
+    private String localName;
+    private String name;
+    private String countryCode;
+    private boolean fixed;
+    private boolean global;
+    private List<String> counties;
+    private Integer launchYear;
+    private String type;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public boolean isFixed() {
+        return fixed;
+    }
+
+    public void setFixed(boolean fixed) {
+        this.fixed = fixed;
+    }
+
+    public boolean isGlobal() {
+        return global;
+    }
+
+    public void setGlobal(boolean global) {
+        this.global = global;
+    }
+
+    public List<String> getCounties() {
+        return counties;
+    }
+
+    public void setCounties(List<String> counties) {
+        this.counties = counties;
+    }
+
+    public Integer getLaunchYear() {
+        return launchYear;
+    }
+
+    public void setLaunchYear(Integer launchYear) {
+        this.launchYear = launchYear;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
