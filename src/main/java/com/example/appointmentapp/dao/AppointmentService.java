fccb225da53de73283ce84b3eeef3bbda6a02160
gdivovic <goran.divovic@audatex.com>
2023-05-14 14:22:51 +0200

    Add Javadoc in AppointmentService

--- src/main/java/com/example/appointmentapp/dao/AppointmentService.java
+++ src/main/java/com/example/appointmentapp/dao/AppointmentService.java
@@ -17,19 +17,29 @@ import java.util.Optional;
  * @author goran.divovic
  */
 @Singleton
-public class AppointmentDao {
+public class AppointmentService {
 
     private static MongoClient mongoClient;
     private static MongoDatabase database;
     private static MongoCollection<Document> collection;
+    private static final String DB_NAME = "appointmentdb";
+    private static final String DB_COLLECTION_NAME = "appointment";
 
     @PostConstruct
     public void init() {
+        //todo: @goran.divovic change locahost to mongo to provide setup for docker
         mongoClient = MongoClients.create("mongodb://localhost:27017");
-        database = mongoClient.getDatabase("appointmentdb");
-        collection = database.getCollection("appointment");
+        database = mongoClient.getDatabase(DB_NAME);
+        collection = database.getCollection(DB_COLLECTION_NAME);
     }
 
+    /**
+     * Find ({@link Appointment} by given id.
+     *
+     * @param id String Identifier of Appointment
+     *
+     * @return {@link Optional} value of found {@link Document}
+     * */
     public Optional<Document> findById(String id) {
         try {
             ObjectId objectId = new ObjectId(id);
@@ -39,6 +49,11 @@ public class AppointmentDao {
         }
     }
 
+    /**
+     * Find all appointments {@link Appointment} in database.
+     *
+     * @return list of appointments
+     * */
     public List<Appointment> findALl() {
         List<Appointment> appointmentList = new ArrayList<>();
         for (Document document : collection.find()) {
@@ -47,14 +62,30 @@ public class AppointmentDao {
         return appointmentList;
     }
 
+    /**
+     * Creates new {@link Appointment} in database.
+     *
+     * @param appointment Appointment object that will be saved in database.
+     * */
     public void save(Appointment appointment) {
         collection.insertOne(AppointmentDocumentMapper.toDocument(appointment));
     }
 
+    /**
+     * Updates {@link Document}, replacing old values with passed new one.
+     *
+     * @param existingDocument Existing document that will be updated.
+     * @param updatedDocument Document that contains updated values.
+     * */
     public void update(Document existingDocument, Document updatedDocument) {
         collection.replaceOne(existingDocument, updatedDocument);
     }
 
+    /**
+     * Deletes one {@link Document} from database.
+     *
+     * @param document Document that will be deleted from database.
+     * */
     public void delete(Document document) {
         collection.deleteOne(document);
     }
